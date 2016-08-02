9e92a7d211bd4a4d83e51e251b860d7348943f17
s14007 <s14007@std.it-college.ac.jp>
2016-08-03 01:32:54 +0900

    SpinnerTestProductNum

--- app/src/main/java/com/example/s14007/myapplication/MainActivity.java
+++ app/src/main/java/com/example/s14007/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.s14007.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Test", "Clear");
-        Log.e("log:", "test");
-        Log.e("Log:", "SummerVacation");
-        Log.e("Log:", "test2");
+        // Listの作成
+        ArrayList<String> list = new ArrayList<String>();
+        // Listにデータを入れる
+        /*list.add("りゅう");
+        list.add("へび");
+        list.add("うま");
+        list.add("ひつじ");*/
+
+        for (int i = 0; i < 100; i++) {
+            list.add(String.valueOf(i));
+        }
+
+        // Adapterの作成
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        // ドロップダウンのレイアウトを指定
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // ListViewにAdapterを関連付ける
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
     }
 }
